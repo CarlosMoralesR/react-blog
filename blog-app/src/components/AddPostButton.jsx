78d30e717c1e0a3f5a2c5a95dfb9b72fb302390e
CarlosMoralesR <carlangetas007@hotmail.com>
2024-01-24 00:14:15 -0600

    fix(details): fixing styles and modifying add post button

--- blog-app/src/components/AddPostButton.jsx
+++ blog-app/src/components/AddPostButton.jsx
@@ -1,11 +1,18 @@
+import AddIcon from "@mui/icons-material/Add";
+import { Tooltip } from "react-tooltip";
+
 const AddPostButton = ({ onClick }) => {
   return (
     <div className="flex justify-end p-4 mx-6">
       <button
-        className="bg-[#f7d571] text-black py-2 px-4 rounded hover:bg-[#f1c94e] hover:shadow-xl"
+        className="bg-[#51d9e0] text-black h-12 w-12 lg:py-1 lg:px-3 rounded hover:bg-[#6fe1e7] hover:shadow-xl"
         onClick={onClick}
+        data-tooltip-id="add-post-tooltip"
+        data-tooltip-content="Add Post-It"
+        data-tooltip-place="left"
       >
-        Add Post
+        <AddIcon></AddIcon>
+        <Tooltip id="add-post-tooltip" />
       </button>
     </div>
   );
--- blog-app/src/components/Navbar.jsx
+++ blog-app/src/components/Navbar.jsx
@@ -1,25 +1,29 @@
 import avatar from "../assets/avatar.png";
 import froggy from "../assets/froggy.png";
+import LogoutIcon from "@mui/icons-material/Logout";
 
 const Navbar = () => {
   return (
     <nav className="bg-gradient-to-r from-slate-700 via-purple-800 to-slate-700 p-10">
       <div className="flex items-center justify-between">
-        <div className="flex  items-center text-white font-bold text-3xl">
+        <div className="flex  items-center text-white  font-bold text-3xl">
           <img
             src={froggy}
             alt="User Avatar"
-            className="w-12 h-12 rounded-full object-cover mr-5"
+            className="w-14 h-14  rounded-full object-cover mr-5 transition-all duration-100 ease-out hover:w-16 hover:h-16"
           />
-          Welcome to Bloggy!
+          <p className="hover:text-black">Welcome to Bloggy!</p>
         </div>
         <div className="flex items-center">
-          <div className="text-white mr-8 text-xl">carlosm29</div>
+          <div className="text-white hover:text-black mr-8 text-xl hover:shadow-2xl">
+            carlosdmr29
+          </div>
           <img
             src={avatar}
             alt="User Avatar"
-            className="w-12 h-12 rounded-full object-cover"
+            className="w-14 h-14 rounded-full object-cover cursor-pointer hover:opacity-80 hover:shadow-2xl"
           />
+          <LogoutIcon className="ml-6 cursor-pointer hover:opacity-70 hover:shadow-lg"></LogoutIcon>
         </div>
       </div>
     </nav>
